b2b93f94fb89fb02d99cd216b7ff163393caffc4
ث rbnwonknui <iunknownbrp@gmail.com>
2025-09-18 14:08:13 -0300

    Update ShowAwnsers.js

--- Versions/ShowAwnsers/ShowAwnsers.js
+++ Versions/ShowAwnsers/ShowAwnsers.js
@@ -1,3 +1,13 @@
+console.clear();
+console.log("By rbnwonknui");
+
+function playAudio(url) {
+  const audio = new Audio(url);
+  audio.play().catch(e => console.log('Audio play failed:', e));
+}
+
+playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/gcelzszy.wav');
+
 const url = window.location.href.replace(/\/$/, "");
 const lang = url.split("/").pop();
 
